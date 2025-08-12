1cfb9f027a75213b09840c0cc4684effa2e354e4
정주상님/New Solution&QA팀 <jusang.jung@sk.com>
2025-08-12 14:05:10 +0900

    add firebase settings by config path to application properties

--- api/src/main/java/com/example/openrunapi/config/FirebaseConfig.java
+++ api/src/main/java/com/example/openrunapi/config/FirebaseConfig.java
@@ -6,34 +6,35 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
 
-    private static final String FIREBASE_CONFIG_PATH = "openrun-ed7a1-firebase-adminsdk-fbsvc-4e67d9a55a.json";
+    @Value("${firebase.config.path}")
+    private String firebaseConfigPath;
 
     @PostConstruct
     public void initialize() {
         try {
-            if (FirebaseApp.getApps().isEmpty()) { // 이미 초기화되었는지 확인
-                InputStream serviceAccount = new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
 
                 FirebaseOptions options = FirebaseOptions.builder()
                         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                         .build();
 
                 FirebaseApp.initializeApp(options);
-                log.info("Firebase application has been initialized");
+                log.info("Firebase application has been initialized from path: {}", firebaseConfigPath);
             }
         } catch (IOException e) {
-            log.error("Error initializing Firebase.", e);
+            log.error("Error initializing Firebase from path: {}", firebaseConfigPath, e);
             throw new RuntimeException("Error initializing Firebase.", e);
         }
     }
