04364c10c046df3e107e8e3d29e08f54f10bd3c1
정주상 <dev.openrun@gmail.com>
2026-03-12 11:37:21 +0900

    robots.txt, sitemap.xml 추가

---
+++ api/src/main/java/com/example/openrunapi/domain/common/controller/RobotsController.java
@@ -0,0 +1,18 @@
+package com.example.openrunapi.domain.common.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RobotsController {
+
+    @GetMapping(value = "/robots.txt", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String robots() {
+        return """
+                User-agent: *
+                Disallow: /api/
+                Allow: /
+                """;
+    }
+}
